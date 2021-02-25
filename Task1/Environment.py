c9b6a28105920caa6b2aa6b998e157e92d9ad6ed
Tommaso Capecchi <tommycaps@hotmail.it>
2021-02-25 12:47:41 +0000

    Added docstring

--- Task1/Environment.py
+++ Task1/Environment.py
@@ -4,6 +4,10 @@ import enum
 
 
 class EnvironmentUtils(enum.IntEnum):
+    """
+    This class represents a series of utils used to display
+    the board on the console.
+    """
     FREE_LOCATION = 0
     WALL = 1
     AGENT = 2
@@ -14,8 +18,20 @@ class EnvironmentUtils(enum.IntEnum):
 
 
 class Environment:
+    """
+    This class represents the environment of the game.
+    It is responsible for the creation of the starting environment of the game. The class also
+    handles the actions taken by the agent, updating the board accordingly.
+    """
 
     def __init__(self, N=20, n_enemies=10, n_gold=15, n_bombs=10):
+        """
+        Inits the environment of the board.
+        :param N : The dimension of the board. It should be a square board of (N,N). Default = 20
+        :param n_enemies : The number of enemies in the game. Default = 10
+        :param n_gold : The number of gold resources in the game. Default = 15
+        :param n_bombs : The number of bombs in the game. Default = 10
+        """
         self.dimension = N
         self.board = self.init_board()
         self.agent = self.init_agent()
@@ -26,12 +42,23 @@ class Environment:
         self.display_board()
 
     def init_agent(self):
+        """
+        This function first search for a valid location for the agent, then instantiates the agent
+        with the valid location. Note that the agent at the start of the game can only spawn in
+        one of the possible columns of the the lower row of the board (excluding the walls).
+        :return agent: The agent of the environment:
+        """
         agent_location = (self.dimension-2, np.random.randint(1, self.dimension-1))
         agent = Agent(agent_location)
         self.board[agent_location[0], agent_location[1]] = EnvironmentUtils.AGENT
         return agent
 
     def init_board(self):
+        """
+        This function set up the walls for the board.
+        :return board: The board of dimension (N,N):
+
+        """
         board = np.zeros((self.dimension, self.dimension))
         board[:1, :] = EnvironmentUtils.WALL
         board[:, :1] = EnvironmentUtils.WALL
@@ -40,11 +67,22 @@ class Environment:
         return board
 
     def generate_flag(self):
+        """
+        This function generate a valid location for the flag. It then updates the board accordingly.
+        The flag at the start of the game can only spawn in one of the possible columns of the the upper
+        row of the board (excluding the walls).
+        :return flag_location : The x,y coordinates of the flag.:
+        """
         flag_location = (1, np.random.randint(1, self.dimension-1))
         self.board[flag_location[0], flag_location[1]] = EnvironmentUtils.FLAG
         return flag_location
 
     def generate_enemies(self, n_enemies):
+        """
+        Generates the enemies locations and updates the board accordingly.
+        :param n_enemies : The number of enemies to generate.
+        :return enemies_locations : The list of all the enemies locations
+        """
         enemies_locations = []
         for _ in range(n_enemies):
             x, y = self.generate_envir_resource()
@@ -53,6 +91,11 @@ class Environment:
         return enemies_locations
 
     def generate_gold(self, n_gold):
+        """
+        Generates the gold locations and updates the board accordingly.
+        :param n_gold: The number of gold resources to generate.
+        :return gold_locations: The list of all the gold resources locations.
+        """
         gold_locations = []
         for _ in range(n_gold):
             x, y = self.generate_envir_resource()
@@ -61,6 +104,11 @@ class Environment:
         return gold_locations
 
     def generate_bombs(self, n_bombs):
+        """
+        Generates the bombs locations and updates the board accordingly.
+        :param n_bombs: The number of bombs to generate.
+        :return n_bombs: The list of all the bombs locations.
+        """
         bombs_locations = []
         for _ in range(n_bombs):
             x, y = self.generate_envir_resource()
@@ -69,6 +117,10 @@ class Environment:
         return bombs_locations
 
     def generate_envir_resource(self):
+        """
+        This function generates a tuple of valid coordinates in the board.
+        :return x_coord, y_coord: The pair of valid coordinates.
+        """
         while True:
             x_coord = np.random.randint(2, self.dimension - 2)
             y_coord = np.random.randint(1, self.dimension - 1)
@@ -77,6 +129,9 @@ class Environment:
         return x_coord, y_coord
 
     def display_board(self):
+        """
+        This function displays the board.
+        """
         display_helper = {0: '.',
                           1: 'X',
                           2: 'A',
