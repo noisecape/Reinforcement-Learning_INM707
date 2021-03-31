Bug solved. No gradient was computed on the Actor model.

Task1/environment.py
@@ -2,12 +2,19 @@ import numpy as np
 from utils import Environment, GameDifficulty, Experiment
 from policy import E_Greedy
 import matplotlib.pyplot as plt
+import os, pickle
+
 
 def train(episode, env, lr, gamma, policy):
     reward_history = []
     for episode in range(episode):
         running_reward = []
+        seed = 0
         while not env.is_gameover:
+            # to be able to replicate the same experiment while
+            # keeping the stochastic component in the environment
+            np.random.seed(seed)
+            seed += 1
             # choose action
             idx_action = policy.take_action(env.agent.current_location, env.q_values)
             # store previous coordinates of the agent
@@ -26,32 +33,54 @@ def train(episode, env, lr, gamma, policy):
             env.q_values[prev_x, prev_y, idx_action] = updated_q_value
         # at the end of each game store the average reward
         running_reward.append(env.final_reward)
-        if episode % 100 == 0:
+        if episode % 10 == 0:
             avg_reward = np.mean(running_reward)
             reward_history.append(avg_reward)
         env.reset()
-    return reward_history
+    return reward_history, env.q_values
 
 
 ## HYPERPARAMETERS
 
-episodes = 1000
-experiments = [Experiment(episodes=episodes, difficulty=GameDifficulty.EASY),
-               Experiment(episodes=episodes, difficulty=GameDifficulty.HARD, lr=0.001, gamma=0.99)]
-exp_results = {}
-for idx, exp in enumerate(experiments):
-    history_rewards = []
-    history_q_values = []
-    print('Beginning experiment [{}/{}] ...'.format(idx+1, len(experiments)))
-    dim_batch = [10, 15, 20, 25]
-    for dim in dim_batch:
-        env = Environment(dimension=dim, difficulty=exp.difficulty)
-        policy = E_Greedy(exp.epsilon)
-        rewards = train(episode=exp.episodes, env=env, lr=exp.lr, gamma=exp.gamma, policy=policy)
-        history_rewards.append(rewards)
 
-    exp_results[exp.difficulty.name] = {'rewards': history_rewards, 'dim': dim_batch}
-    print('Experiment [{}/{}] completed'.format(idx, len(experiments)))
+# hyperparameters
+episodes = 10000
+gamma = 0.96
+dim_batch = [10, 15, 20, 25]
+experiments = [Experiment(episodes=episodes, difficulty=GameDifficulty.EASY, gamma=gamma),
+               Experiment(episodes=episodes, difficulty=GameDifficulty.HARD, gamma=gamma)]
+
+exp_results = {}
+save_path = os.path.join(os.curdir, 'saved_files/difficulty_comparison.pickle')
+if not os.path.exists(os.path.join(os.curdir, 'saved_files')):
+    os.mkdir(os.path.join(os.curdir, 'saved_files'))
+
+if os.path.exists(save_path):
+    # load values
+    with open(save_path, 'rb') as file2load:
+        print('Loading previous values...')
+        exp_results = pickle.load(file2load)
+        print('Values Loaded')
+
+else:
+    for idx, exp in enumerate(experiments):
+        history_rewards = []
+        history_q_values = []
+        print('Beginning experiment [{}/{}] ...'.format(idx+1, len(experiments)))
+        for dim in dim_batch:
+            env = Environment(dimension=dim, difficulty=exp.difficulty)
+            policy = E_Greedy(exp.epsilon)
+            rewards, q_values = train(episode=exp.episodes, env=env, lr=exp.lr, gamma=exp.gamma, policy=policy)
+            history_rewards.append(rewards)
+            history_q_values.append(q_values)
+        exp_results[exp.difficulty.name] = {'rewards': history_rewards, 'dim': dim_batch, 'q_values': history_q_values}
+        print('Experiment [{}/{}] completed'.format(idx+1, len(experiments)))
+    print('Training Completed')
+    file_name = 'difficulty_comparison.pickle'
+    print('Saving values...')
+    with open(os.path.join(os.curdir, 'saved_files/'+file_name), 'wb') as file2store:
+        pickle.dump(exp_results, file2store, protocol=pickle.HIGHEST_PROTOCOL)
+    print('Done!')
 
 
 def plot_image(exp_results, exp_name):
@@ -69,46 +98,122 @@ def plot_image(exp_results, exp_name):
     plt.subplots_adjust(wspace=0.4, hspace=0.4)
     plt.show()
 
+# plot easy episodes
+plot_image(exp_results, exp_name='EASY')
+# plot hard episodes
+plot_image(exp_results, exp_name='HARD')
 
-for exp_name in ['EASY', 'HARD']:
-    plot_image(exp_results, exp_name)
+# Check how the reward behaves when the dimensions and the eps-values change
+# Set an eps value and a dimension. Execute experiments, compute avg to see
+# how different eps values affect rewards in different sizes.
+
+# Hyperparameters
+eps_values = [.01, .1, .2, .5]
+dim = 20
+results = {}
+gamma = .96
+episodes = 100
+# in a stochastic environment we want the agent to learn little by little
+# lr = 0 -> no learning
+# lr = 1 -> learn only the most recent information
+lr = .2
+
+save_path = os.path.join(os.curdir, 'saved_files/eps_comparison.pickle')
+if os.path.exists(save_path):
+    # load values
+    with open(save_path, 'rb') as file2load:
+        print('Loading previous values...')
+        results = pickle.load(file2load)
+        print('Values Loaded')
+else:
+    for idx, eps in enumerate(eps_values):
+        print('Beginning experiment [{}/{}] ...'.format(idx+1, len(eps_values)))
+        reward_history = []
+        name_exp = 'Experiment {}'.format(idx+1)
+        policy = E_Greedy(eps)
+        env = Environment(dimension=dim, difficulty=GameDifficulty.EASY)
+        rewards, _ = train(episodes, env, lr=lr, gamma=gamma, policy=policy)
+        reward_history.append(rewards)
+        results[name_exp] = {'rewards': reward_history, 'eps': eps_values}
+        print('Experiment [{}/{}] completed'.format(idx+1, len(eps_values)))
+    file_name = 'eps_comparison.pickle'
+    with open(os.path.join(os.curdir, 'saved_files/'+file_name), 'wb') as file2store:
+        pickle.dump(results, file2store, protocol=pickle.HIGHEST_PROTOCOL)
 
 
-# # plot results
-# plt.figure(figsize=(8, 8))
-# plt.subplot(2, 2, 1)
-# plt.plot(exp_results['EASY']['rewards'][0])
-# plt.xscale('log')
-# plt.xlabel('Episodes')
-# plt.ylabel('Rewards')
-# plt.title('{} Results, Dimension {}'.format('EASY', 10))
-#
-#
-# plt.subplot(2, 2, 2)
-# plt.plot(exp_results['EASY']['rewards'][1])
-# plt.xscale('log')
-# plt.xlabel('Episodes')
-# plt.ylabel('Rewards')
-# plt.title('{} Results, Dimension {}'.format('EASY', 20))
-#
-#
-# plt.subplot(2, 2, 3)
-# plt.plot(exp_results['EASY']['rewards'][2])
-# plt.xscale('log')
-# plt.xlabel('Episodes')
-# plt.ylabel('Rewards')
-# plt.title('{} Results, Dimension {}'.format('EASY', 30))
-#
-#
-# plt.subplot(2, 2, 4)
-# plt.plot(exp_results['EASY']['rewards'][3])
-# plt.xscale('log')
-# plt.xlabel('Episodes')
-# plt.ylabel('Rewards')
-# plt.title('{} Results, Dimension {}'.format('EASY', 40))
-#
-# plt.subplots_adjust(wspace=0.4, hspace=0.4)
-#
-# plt.show()
+# function used to plot results
+def plot_eps(results):
+    plt.figure(figsize=(10, 8))
+    for idx, name in enumerate(results.keys()):
+        data = results[name]['rewards'][0]
+        eps = results[name]['eps'][idx]
+        plt.subplot(2, 2, idx+1)
+        plt.plot(data)
+        plt.xscale('log')
+        plt.xlabel('Reward Samples')
+        plt.ylabel('Average Rewards')
+        plt.title('{} Results, eps {}'.format(name, eps))
+    plt.subplots_adjust(wspace=0.4, hspace=0.4)
+    plt.show()
+
+
+# plot rewards vs eps values.
+plot_eps(results)
+
+# inference stage
+
+n_games = 500
+dim = [10, 15, 20, 25]
+difficulty = GameDifficulty.EASY.name
+inference_results = {}
+
+print('Start Experiment')
+# iterate through all the desired dimensions
+for idx in range(len(dim)):
+    # init environment
+    env = Environment(dim[idx])
+    # init name of experiment used to store final info
+    # retrieve q_values from previous training
+    q_values = exp_results[difficulty]['q_values'][idx]
+    winnings = []
+    losses = []
+    # iterate through all the games
+    for game in range(n_games):
+        env.reset()
+        limit_steps = 0
+        while not env.is_gameover:
+            # get agent location
+            agent_x, agent_y = env.agent.current_location
+            # get action idx
+            action_idx = np.argmax(q_values[agent_x][agent_y])
+            # take a step and get reward
+            _ = env.step(action_idx)
+            limit_steps += 1
+            if limit_steps > 1000:
+                env.is_gameover = True
+        # check if the agent won
+        if env.goal_reached:
+            winnings.append(env.final_reward)
+        else:  # the agent lost the game
+            losses.append(env.final_reward)
+        # every 100 games print some useful statistics
+        if game % 100 == 0:
+            print('Environment {}, Games [{}]/[{}] executed. Wins: {}, Losses: {}'.format(difficulty,
+                                                                                          game+1,
+                                                                                          n_games,
+                                                                                          len(winnings),
+                                                                                          len(losses)))
+    avg_reward_wins = np.mean(winnings)
+    avg_reward_losses = np.mean(losses)
+    inference_results[difficulty] = {'Wins': len(winnings),
+                                     'Average_reward': avg_reward_wins,
+                                     'Losses': len(losses),
+                                     'Average_reward': avg_reward_losses}
+
+    print('Environment {}, Total wins: {}, average reward: {}, Total losses: {}, average reward: {}'.format(difficulty,
+                                                                                                            len(winnings),
+                                                                                                            avg_reward_wins,
+                                                                                                            len(losses),
+                                                                                                            avg_reward_losses))
 
 

Task1/utils.py
@@ -47,10 +47,10 @@ class Rewards(enum.IntEnum):
     depending on the type of location.
     """
     FREE = -1
-    WALL = -2
-    EXIT_ROAD_SECTION = 25
-    CAR = -7
-    SAFE = 150
+    WALL = -5
+    EXIT_ROAD_SECTION = 10
+    CAR = -75
+    SAFE = 100
 
 
 class GameDifficulty(enum.Enum):
@@ -67,6 +67,7 @@ class Colors:
     AGENT_BLUE = '\033[94m'
     SAFE_GREEN = '\033[92m'
     CAR_RED = '\033[91m'
+    EXIT_ROAD = '\033[33m'
     ENDC = '\033[0m'
 
 
@@ -92,7 +93,7 @@ class Experiment:
                  difficulty=GameDifficulty.EASY,
                  epsilon=.1,
                  gamma=.9,
-                 lr=.01):
+                 lr=.2):
 
         self.episodes = episodes
         self.dimension = dimension
@@ -115,10 +116,10 @@ class Environment:
     # probability of not creating it.
     # The second element represents how wide the road section is. The harder the wider.
 
-    ROADS_EASY = {'prob_road': [0.4, 0.6], 'width': 1, 'traffic': [0.95, 0.05]}
-    ROADS_MEDIUM = {'prob_road': [0.4, 0.6], 'width': 1, 'traffic': [0.95, 0.05]}
-    ROADS_HARD = {'prob_road': [0.4, 0.6], 'width': 2, 'traffic': [0.92, 0.08]}
-    ROADS_EXTREME = {'prob_road': [0.5, 0.5], 'width': 2, 'traffic': [0.85, 0.15]}
+    ROADS_EASY = {'prob_road': [0.55, 0.45], 'width': 1, 'traffic': [0.7, 0.3]}
+    ROADS_MEDIUM = {'prob_road': [0.55, 0.45], 'width': 1, 'traffic': [0.7, 0.3]}
+    ROADS_HARD = {'prob_road': [0.5, 0.5], 'width': 2, 'traffic': [0.7, 0.3]}
+    ROADS_EXTREME = {'prob_road': [0.5, 0.5], 'width': 2, 'traffic': [0.60, 0.4]}
 
     # Defines the possible actions
     Action = namedtuple('Action', ['id', 'name', 'idx_i', 'idx_j'])
@@ -139,6 +140,7 @@ class Environment:
         self.dimension = dimension
         self.difficulty = difficulty
         self.is_gameover = False
+        self.goal_reached = False
         # generate board with walls
         self.board = self.init_board()
         # generate road sections
@@ -271,13 +273,13 @@ class Environment:
         while board_index > 3:
             if board_index % 2 == 0 and self.board[board_index + 1, 1] != EnvironmentUtils.ROAD:
                 prob_index = np.argmax(np.random.multinomial(1, prob_generate_road))
-                if prob_index == 0:  # generate road section
+                if prob_index == 1:  # generate road section
                     for n_road_lane in range(road_width):
                         self.board[board_index, 1:-1] = EnvironmentUtils.ROAD
                         if n_road_lane == road_width-1:
                             board_index -= 1
                             self.board[board_index, 1:-1] = EnvironmentUtils.EXIT_ROAD
-                        board_index -= 1
+                board_index -= 1
             else:
                 board_index -= 1
 
@@ -303,7 +305,7 @@ class Environment:
                           3: ('C', Colors.CAR_RED),
                           4: ('R', Colors.ENDC),
                           5: ('S', Colors.SAFE_GREEN),
-                          6: ('E', Colors.SAFE_GREEN)
+                          6: ('E', Colors.EXIT_ROAD)
                           }
 
         print('Difficulty: {}'.format(self.difficulty.name))
@@ -314,6 +316,7 @@ class Environment:
 
     def reset(self):
         self.is_gameover = False
+        self.goal_reached = False
         # generate board with walls
         self.board = self.init_board()
         # generate road sections
@@ -407,6 +410,7 @@ class Environment:
             self.agent.current_location = prev_x, prev_y
         elif self.board[new_x][new_y] == EnvironmentUtils.SAFE:
             self.is_gameover = True
+            self.goal_reached = True
             self.final_reward += reward
             return reward
         elif self.board[new_x][new_y] == EnvironmentUtils.ROAD:

Task3/main.py
@@ -1,9 +1,8 @@
 import gym
-from Task3.model import ActorModel, CriticModel, Memory
+from Task3.model import ActorModel, CriticModel, Memory, PPO
 import torch
 import os
 import numpy as np
-from torch.distributions.categorical import Categorical
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
@@ -12,114 +11,31 @@ if torch.cuda.is_available():
     device = torch.device('cuda')
 print(device)
 
-### USEFUL METHODS
-
-
-def init_memory():
-    states = []
-    actions = []
-    rewards = []
-    values = []
-    is_done = []
-    log_probs = []
-    memory = Memory(states, actions, rewards, values, is_done, log_probs)
-    return memory
-
-
-def get_action(act_prob):
-    result = []
-    for idx, probs in enumerate(act_prob):
-        action_idx = np.argmax(np.random.multinomial(1, np.array(probs.data)))
-        new_prob = probs[action_idx]
-        result.append(new_prob.item())
-    result = torch.tensor(np.array(result))
-    return result
-
-
-def ppo(n_epochs, gamma, clip_ratio, labda, c_1, T):
-    """
-    This function perform an update of the policy
-    based on the trajectory stored in memory. The algorithm
-    applied is the Actor-Critic style described by the paper
-    https://arxiv.org/pdf/1707.06347.pdf
-    :return:
-    """
-    # convert properly all the data structures
-    states, actions, rewards, values, is_done, log_probs = memory.convert_np_arrays()
-    states = torch.tensor(states, dtype=torch.float).to(device)
-    values = torch.tensor(values, dtype=torch.float).to(device)
-    log_probs = torch.tensor(log_probs, dtype=torch.float).to(device)
-    actions = torch.tensor(actions).to(device)
-
-    for e in range(n_epochs):
-        # randomly generate batches
-        returns = []
-        disc_reward = 0
-        # compute discounted reward using MC method.
-        for idx, rew in enumerate(reversed(rewards)):
-            disc_reward = rewards[idx] + (gamma * disc_reward)
-            returns.insert(0, disc_reward)
-
-        returns = torch.tensor(returns)
-        advantages = returns - values
-        advantages = torch.tensor(np.array(advantages))
-
-        # feedforward pass to the actor net to get the prob distr. of
-        # each action per batch
-        act_prob = actor(states)
-        act_prob = Categorical(act_prob)
-        # feedforward pass to the critic net to get the values per batch
-        values_ = critic(states).squeeze()
-        # compute new prob log.
-        new_prob = act_prob.log_prob(actions)
-        # in order to get the correct ratio the exp() operator must be applied
-        # because of the log value.
-        ratio = torch.exp(new_prob - log_probs)
-        # represents the first term in the L_CLIP equation
-        clip = torch.clamp(ratio, 1 - clip_ratio, 1 + clip_ratio) * advantages
-        # multiply ratio and advantages to get the 'weighted probs'
-        weighted_prob = advantages * ratio
-        # compute the L_CLIP function. Because the
-        # gradient ascent is applied, we have to change sign
-        # take the mean because it's an expectation. This is
-        # the actor loss
-        l_clip = torch.mean(-torch.min(weighted_prob, clip))
-        # compute the L_VF, which is the critic loss
-        l_vf = torch.mean((returns - values_)**2)
-        # combine the two losses to get the final loss L_CLIP_VF
-        total_loss = l_clip + (c_1 * l_vf)
-
-        # perform backprop
-        actor.optimizer.zero_grad()
-        critic.optimizer.zero_grad()
-        total_loss.backward()
-        actor.optimizer.step()
-        critic.optimizer.step()
-    # at the end of the epoch clear the trajectory
-    # in the memory
-    memory.empty_memory()
-
-
 # define some hyperparameters from the paper of PPO
-n_epochs = 50
-gamma = 0.99
-clip_ratio = 0.2
-ldba = 0.97
+n_epochs = 10
+gamma = 0.97
+clip_ratio = 0.15
 c_1 = 0.5
-games = 10000
-fc_size = 256
-T = 10
+games = 1000
+fc_size = 128
+T = 128
 step = 0
+wins = []
+losses = []
 
 
 # create the environment from gym
 env = gym.make('CartPole-v1')
 observation_space = env.observation_space.shape[-1]
 action_space = env.action_space.n
+# create the model
 actor = ActorModel(observation_space, action_space, fc_size)
 critic = CriticModel(observation_space, fc_size)
-memory = init_memory()
-score_history = []
+memory = Memory()
+ppo = PPO(memory, actor, critic)
+
+running_history = []
+
 ## main loop ##
 
 for g in range(games):
@@ -127,47 +43,40 @@ for g in range(games):
     state = env.reset()
     # setup useful variables
     running_score = 0
-    running_history = []
-    score_history = []
     train_counter = 0
     done = False
     # iterate through the game util it's finished
     while not done:
-        # convert state to a tensor.
-        # Add a dimension to support the batch for later steps
-        env.render()
-        state = torch.tensor(state, dtype=torch.float32)
-        # return the actions prod. distribution
-        act_distr = actor(state).squeeze(0)
-        act_distr = Categorical(act_distr)
-        # action = np.argmax(np.random.multinomial(1, np.array(act_distr.data)))
-        action = act_distr.sample()
-        # use the distribribution to sample from a multinomial
-        # distribution. This will ensure that the agent always acts
-        # in a stochastic manner according to the latest version
-        # of the stochastic policy
-        # take a step in the environment
-        new_state, reward, done, info = env.step(action.item())
+        # env.render()
+        act_distr = actor(torch.tensor(state, dtype=torch.float))
+        action = np.argmax(np.random.multinomial(1, np.array(act_distr.data)))
+        new_state, reward, done, info = env.step(action)
         step += 1
         # update running score
         running_score += reward
         # compute log_probs of the prob distribution
-        # log_prob = np.log(act_distr.data[action]).item()
-        log_prob = torch.squeeze(act_distr.log_prob(action)).item()
+        log_prob = np.log(act_distr.data[action]).item()
         # compute the value for this game iteration
-        value = critic(state).data.item()
+        value = critic(torch.tensor(state, dtype=torch.float)).data.item()
         # store the info in the memory
-        memory.push(new_state, action, reward, value, done, log_prob)
+        memory.push(state, action, reward, value, done, log_prob)
+        # update policy once every T steps
         if step % T == 0:
-            ppo(n_epochs, gamma, clip_ratio, ldba, c_1, T)
+            ppo(n_epochs, gamma, clip_ratio, c_1)
             train_counter += 1
         # update current state to new state
         state = new_state
         running_history.append(running_score)
-
-    # game finished, train phase
-    # perform ppo algorithm
-    print('{} game completed, final score: {}, average score {}, training_phase {}'.format(g+1,
-                                                                                           running_score,
-                                                                                           np.mean(running_history),
-                                                                                           train_counter))
+        # by documentation, the problem is solved
+        # if the average reward is greater than 195 over the last 100 trials
+        # let't check this
+        if step % 100 == 0:
+            avg_reward = np.mean(running_history[-100:])
+            if avg_reward >= 195:
+                wins.append(avg_reward)
+            else:
+                losses.append(avg_reward)
+            print('Game completed: [{}]/[{}],'
+                  ' Average reward of last 100 trials: {}'.format(g+1,
+                                                                  games,
+                                                                  avg_reward))

Task3/model.py
@@ -1,69 +1,64 @@
 import torch
 import torch.nn as nn
-import numpy as np
-from collections import namedtuple
 import torch.optim as opt
-from torch.distributions.categorical import Categorical
-
-Trajectory = namedtuple('Trajectory', 'states, actions, rewards, values, is_done, log_probs')
 
 device = torch.device('cpu')
 if torch.cuda.is_available():
     device = torch.device('cuda')
 print(device)
 
-torch.autograd.set_detect_anomaly(True)
 
 class Memory:
-
-    def __init__(self, states, actions, rewards, values, is_done, log_probs):
-        # init data structures to hold trajectories info
-        self.trajectory = Trajectory(states, actions, rewards, values, is_done, log_probs)
-
-    # def create_batch(self, t_len):
-    #     """
-    #     This function creates a batch of dimension 'batch_size' that will be
-    #     used during the training of the agent. The batch is created by
-    #     shuffling a list of indices and extracting 'len_trajectory/batch_size' batches.
-    #     :param batch_dim: the dimension of each batch
-    #     :param t_len: the length of a single trajectory.
-    #     :return:
-    #     """
-    #     indices = np.arange(0, t_len)
-    #     np.random.shuffle(indices)
-    #     index_start = np.arange(0, t_len, self.batch_size)
-    #     # creates the batches
-    #     batches = [indices[i:i + self.batch_size] for i in index_start]
-    #     return batches
-
-    def convert_np_arrays(self):
-        """
-        Useful function to convert the list stored in the memory as
-        numpy array so that they can be easily converted to tensors.
-        :return:
-        """
-        return np.array(self.trajectory.states), \
-               np.array(self.trajectory.actions, dtype=int), \
-               np.array(self.trajectory.rewards), \
-               np.array(self.trajectory.values), \
-               np.array(self.trajectory.is_done, dtype=int), \
-               np.array(self.trajectory.log_probs)
+    """
+    This class implements a mechanism to store required information
+    of a trajectory to update the Policy using the PPO algorithm.
+    """
+    def __init__(self):
+        self.states = []
+        self.actions = []
+        self.rewards = []
+        self.values = []
+        self.is_done = []
+        self.log_probs = []
 
     def push(self, states, actions, rewards, values, is_done, log_probs):
-        self.trajectory.states.append(states)
-        self.trajectory.actions.append(actions)
-        self.trajectory.rewards.append(rewards)
-        self.trajectory.values.append(values)
-        self.trajectory.is_done.append(is_done)
-        self.trajectory.log_probs.append(log_probs)
+        """
+        This class push the information into memory
+        :param states: the states of the environment
+        :param actions: the actions taken by the agent
+        :param rewards: the rewards gained during each trajectory
+        :param values: the values associated with each state
+        :param is_done: a boolean vector used to mark games that ended.
+        :param log_probs: the log probabilities of each action in a trajectory
+        :return:
+        """
+        self.states.append(states)
+        self.actions.append(actions)
+        self.rewards.append(rewards)
+        self.values.append(values)
+        self.is_done.append(is_done)
+        self.log_probs.append(log_probs)
 
     def empty_memory(self):
-        new_trajectory = Trajectory([], [], [], [], [], [])
-        self.trajectory = new_trajectory
+        """
+        This function emptify the memory.
+        :return:
+        """
+        self.states = []
+        self.actions = []
+        self.rewards = []
+        self.values = []
+        self.is_done = []
+        self.log_probs = []
 
 
 class ActorModel(nn.Module):
-
+    """
+    This class implements the Actor in the PPO algorithm.
+    Its main functionality is to implement the Neural Network
+    which takes in input the state of the environment and outputs
+    the probability distribution associated to each action.
+    """
     def __init__(self, obs_size, action_size, fc_size=64):
         super(ActorModel, self).__init__()
         self.obs_size = obs_size
@@ -77,19 +72,29 @@ class ActorModel(nn.Module):
                                    nn.Linear(fc_size, action_size),
                                    nn.Softmax(dim=-1)
                                    ).to(device)
-        optim_params = {'lr': 0.0001, 'weight_decay': 0.001}
+        optim_params = {'lr': 0.0025}
         self.optimizer = opt.Adam(self.parameters(), **optim_params)
 
     def forward(self, x):
+        """
+        This function takes in input the states of
+        the environment and forward pass it through
+        the Neural Network to output the action probabilities.
+        :param x: state of the environment
+        :return: the probability distribution associated to each possible action.
+        """
         act_prob = self.model(x)
         return act_prob
 
 
 class CriticModel(nn.Module):
-
+    """
+    This class implements the Critic model of the PPO algorithm
+    This model is mainly composed by the Neural Network which takes
+    in input the state of the environment and outputs the V values.
+    """
     def __init__(self, obs_size, fc_size=64):
         super(CriticModel, self).__init__()
-
         self.model = nn.Sequential(nn.Linear(obs_size, fc_size),
                                    nn.ReLU(),
                                    nn.Linear(fc_size, fc_size),
@@ -97,9 +102,91 @@ class CriticModel(nn.Module):
                                    nn.Linear(fc_size, 1)
                                    ).to(device)
 
-        optim_params = {'lr': 0.0001, 'weight_decay': 0.001}
+        optim_params = {'lr': 0.0025}
         self.optimizer = opt.Adam(self.parameters(), **optim_params)
 
     def forward(self, x):
+        """
+        This function forward pass the input through the critic
+        Neural Network in order to calculate the V values.
+        :param x: the states of the environment.
+        :return: the V values
+        """
         value = self.model(x)
         return value
+
+
+class PPO(nn.Module):
+    """
+    This class acts as a container for the Actor model and the Critic model.
+    It wraps their functionalities and merge the two components to implement
+    the PPO algorithm and successfully update the agent's policy.
+    """
+    def __init__(self, memory, actor, critic):
+        super(PPO, self).__init__()
+        self.memory = memory
+        self.actor = actor
+        self.critic = critic
+        self.criterion = nn.MSELoss()
+
+    def forward(self, n_epochs, gamma, clip_ratio, c_1):
+        """
+        This function perform an update of the policy
+        based on the trajectory stored in memory. The algorithm
+        applied is the Actor-Critic style described by the paper
+        https://arxiv.org/pdf/1707.06347.pdf
+        :return:
+        """
+        returns = []
+        disc_reward = 0
+
+        # use the MC method to estimate the return values.
+        for reward, is_terminal in (zip(reversed(self.memory.rewards), reversed(self.memory.is_done))):
+            disc_reward = reward + (gamma * disc_reward) * (1 - (int(is_terminal)))
+            returns.insert(0, disc_reward)
+
+        returns = torch.tensor(returns, dtype=torch.float)
+
+        for e in range(n_epochs):
+            # convert to tensors all the stored information
+            # to forward pass them through the models.
+            states = torch.tensor(self.memory.states, dtype=torch.float).to(device)
+            log_probs = torch.tensor(self.memory.log_probs, dtype=torch.float).to(device)
+            actions = torch.tensor(self.memory.actions).to(device)
+            selected_actions = torch.zeros(actions.shape[0])
+            # compute action probability distribution
+            act_distr = self.actor(states)
+            # select the actions using the action ids stored in the trajectory
+            for idx, act in enumerate(actions):
+                selected_actions[idx] = act_distr[idx][act]
+            # feedforward pass to the critic net to get the values per batch
+            values_ = self.critic(states).squeeze()
+            # compute advantages
+            advantages = returns - values_
+            # compute new prob log.
+            new_prob = torch.log(selected_actions)
+            # in order to get the correct ratio the exp() operator must be applied
+            # because of the log value.
+            ratio = torch.exp(new_prob - log_probs)
+            # represents the first term in the L_CLIP equation
+            clip = torch.clamp(ratio, 1 - clip_ratio, 1 + clip_ratio) * advantages
+            # multiply ratio and advantages to get the 'weighted probs'
+            weighted_prob = advantages * ratio
+            # Compute the L_CLIP function. Gradient ascent is applied,
+            # so we have to change sign. Also take the mean because it's an expectation.
+            # The actor loss
+            l_clip = -torch.min(weighted_prob, clip).mean()
+            # compute the L_VF, which is the critic loss
+            l_vf = self.criterion(returns, values_)
+            # combine the two losses to get the final loss L_CLIP_VF
+            total_loss = l_clip + (c_1 * l_vf)
+
+            # perform backprop
+            self.actor.optimizer.zero_grad()
+            self.critic.optimizer.zero_grad()
+            total_loss.backward()
+            self.actor.optimizer.step()
+            self.critic.optimizer.step()
+        # at the end of the epoch clear the trajectory
+        # in the memory
+        self.memory.empty_memory()